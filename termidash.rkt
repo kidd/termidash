faea6b678b0c1caaf75682ce3802a349afa193f8
Raimon Grau <raimonster@gmail.com>
2017-10-24 14:33:34 +0100

    Returns the correct entry with help of a temporary file

--- termidash.rkt
+++ termidash.rkt
@@ -11,23 +11,33 @@
    #:database sqlite3-db
    #:mode 'read-only))
 
-(define docset-path
+(define docsets-path
   "/home/rgrau/.docsets/~a.docset/Contents/Resources/docSet.dsidx")
 
 (define (docset-path-for name)
-  (format docset-path name))
+  (format docsets-path name))
 
 (define (list-all-entries dbconn)
+  ;; should be private
   (rows-result-rows
    (query dbconn "select name, path from searchIndex")))
 
+(define (counter)
+  (let ((n 0))
+    (lambda ()
+      (set! n (+ n 1))
+      n)))
+
 (define (make-docset name)
+  (define (prepend-name l)
+    (list* (format "~a ~a" name (car l)) (cdr l)))
   (let* ((path (docset-path-for name))
          (conn (connect-to path))
-         (elems (list-all-entries conn)))
+         (elems (map (compose prepend-name vector->list)
+                     (list-all-entries conn))))
     (docset name path conn elems)))
 
-(define docset-names '("AllegroLisp" "Clojure"))
+(define docset-names '("AllegroLisp"))
 
 (define (connections)
   (map make-docset docset-names))
@@ -36,17 +46,37 @@
   (append-map (lambda (x) (docset-elems x))
               (connections)))
 
-(define (-n)
-  (let ((n 0))
-    (map (lambda (x)
-           (set! n (+ 1 n))
-           (string-join (list* (number->string n) (vector->list x)) ))
-         (all-elems))))
+(define (-n l)
+  (let ((c (counter)))
+    (map (lambda (y)
+           (cons (format "~a ~a"
+                          (first y)
+                          (second y))
+                  (third y)))
+         (map (lambda (x)
+                (cons (c) x) )
+              l))))
+
+(define the-hash (make-hash (-n (all-elems))))
 
 (define (get-path-from-str str)
   (list-ref (-n) (string->number (car (string-split str " ")))))
 
+(define (get-all-entries h)
+  (string-join (hash-keys h) "\n"))
+
+(define (store-options opts)
+  (let* ((tmpfile (make-temporary-file))
+         (port (open-output-file tmpfile #:mode 'text #:exists 'truncate)))
+    (display opts port)
+    (close-output-port port)
+    (hash-ref the-hash
+              (string-trim (run-pipeline/out `(cat ,tmpfile) '(dmenu) )))))
+
+
 (define (choose-entry)
   ;(run-pipeline `(,(string-join (-n) "\n")) '(dmenu) `(,get-path-from-str))
-  (run-pipeline `(,tt) '(dmenu) `(,get-path-from-str))
-)
+  (run-pipeline `(,(shellify get-all-entries)) '(dmenu)))
+
+
+; (store-options (get-all-entries the-hash))
